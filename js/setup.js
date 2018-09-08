5e9756e2b23425be34bf36398067bb7e7bf04701
Peter <arcticmonkey07@gmail.com>
2018-09-08 21:37:22 +0300

    Начал делать 4 задание

--- js/setup.js
+++ js/setup.js
@@ -53,3 +53,106 @@
   similarListElement.appendChild(fragment);
 
 })();
+
+// ----------------
+
+(function () {
+  var ESC_KEYCODE = 27;
+  var ENTER_KEYCODE = 13;
+
+  var setupOpen = document.querySelector('.setup-open');
+  var setup = document.querySelector('.setup');
+  var setupClose = document.querySelector('.setup-close');
+
+  var onPopupEscPress = function (evt) {
+    if (evt.keyCode === ESC_KEYCODE) {
+      closePopup();
+    }
+  };
+
+  var openPopup = function () {
+    setup.classList.remove('hidden');
+    document.addEventListener('keydown', onPopupEscPress);
+  };
+
+  var closePopup = function () {
+    setup.classList.add('hidden');
+    document.removeEventListener('keydown', onPopupEscPress);
+  };
+
+  setupOpen.addEventListener('click', function () {
+    openPopup();
+  });
+
+  setupOpen.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === ENTER_KEYCODE) {
+      openPopup();
+    }
+  });
+
+  setupClose.addEventListener('click', function () {
+    closePopup();
+  });
+
+  setupClose.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === ENTER_KEYCODE) {
+      closePopup();
+    }
+  });
+
+  // ---- validation -----
+
+  var userNameInput = setup.querySelector('.setup-user-name');
+
+  userNameInput.addEventListener('invalid', function () {
+    if (userNameInput.validity.tooShort) {
+      userNameInput.setCustomValidity('Имя должно состоять минимум из 2-х символов');
+    } else if (userNameInput.validity.tooLong) {
+      userNameInput.setCustomValidity('Имя не должно превышать 25-ти символов');
+    } else if (userNameInput.validity.valueMissing) {
+      userNameInput.setCustomValidity('Обязательное поле');
+    } else {
+      userNameInput.setCustomValidity('');
+    }
+  });
+
+  userNameInput.addEventListener('input', function (evt) {
+    var target = evt.target;
+    if (target.value.length < 2) {
+      target.setCustomValidity('Имя должно состоять минимум из 2-х символов');
+    } else {
+      target.setCustomValidity('');
+    }
+  });
+
+  // ---------
+
+  var wizardCoat = document.querySelector('.wizard-coat');
+  var wizardEyes = document.querySelector('.wizard-eyes');
+  var wizardFireball = document.querySelector('.setup-fireball-wrap');
+
+
+  var coatColor = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+  var eyes = ['black', 'red', 'blue', 'yellow', 'green'];
+  var fireball = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
+
+
+  function getRandomInt(arr) {
+    var randomInt = Math.floor(Math.random() * arr.length);
+    return randomInt;
+  }
+
+  wizardCoat.addEventListener('click', function () {
+    wizardCoat.style.fill = coatColor[getRandomInt(coatColor)];
+
+  });
+
+  wizardEyes.addEventListener('click', function () {
+    wizardEyes.style.fill = eyes[getRandomInt(eyes)];
+  });
+
+  wizardFireball.addEventListener('click', function () {
+    wizardFireball.style.background = fireball[getRandomInt(fireball)];
+  });
+
+})();
